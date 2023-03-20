846f3d195cb8895ef0370a275f38a2d5e6a05357
dianamishkova <113173126+dianamishkova@users.noreply.github.com>
2023-03-20 15:11:16 -0700

    Add files via upload

---
+++ examples/example4_gr12_MishkovaDiana.c
@@ -0,0 +1,53 @@
+typedef int (*sqlite3_callback)(
+	void*,
+	int,
+	char**,
+	char**
+);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+static int callback(void *data, int argc, char **argv, char **azColName){
+	int i;
+	fprintf(stderr,"%s: ", (const char*)data);
+
+	for(i = 0; i<argc; i++){
+		printf("%s = %s\n" , azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char *arv[]){
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	char *sql;
+	const char *data = "Callback function called";
+	
+	rc = sqlite3_open("test.db", &db);
+	
+	if(rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return(0);
+	} else {
+		fprintf(stderr, "Openes database successfully\n");
+	}
+
+	sql = "SELECT * FROM COMPANY";
+	
+	rc = sqlite3_exec(db,sql,callback,(void*)data, &zErrMsg);
+	
+	if(rc != SQLITE_OK){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	} else{
+		fprintf(stdout, "Operation done successfully\n");
+	}
+	sqlite3_close(db);
+	return 0;
+}
+		
